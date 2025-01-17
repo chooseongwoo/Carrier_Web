d5fb960e6e331ab7763784720febb2c058e49dee
추성우 <cntjddn715@gmail.com>
2025-01-18 00:25:43 +0900

    chore(#4) :: 네비게이션바 아이콘

---
+++ src/widgets/NavigationBar/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as Calendar } from './Calendar';
+export { default as Mail } from './Mail';
+export { default as Diary } from './Diary';
+export { default as Alarm } from './Alarm';
