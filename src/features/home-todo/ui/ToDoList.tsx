197b661521583be0378ffa40f156c4d0c361d54e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-18 17:51:02 +0900

    feat(#6) :: ToDo 퍼블리싱

---
+++ src/features/home-todo/ui/ToDoList.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import {
+  LeftArrow,
+  RightArrow,
+  BtnToDoNormal,
+  BtnToDoChecked,
+} from 'shared/icons';
+import * as s from './style.css';
+
+const ToDoList = () => {
+  const [todoItems, setTodoItems] = useState([
+    { id: 1, text: '미래랑 산책가기', checked: false },
+    { id: 2, text: '하니 산책시키기', checked: true },
+    { id: 3, text: '베이스 치기', checked: false },
+  ]);
+
+  const handleToggle = (id: number) => {
+    setTodoItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  return (
+    <div className={s.ToDoListContainer}>
+      <div className={s.ToDoListHeader}>
+        <div className={s.ToDoListTitle}>해야할 것</div>
+        <div className={s.ToDoListSetDate}>
+          <LeftArrow />
+          <div className={s.ToDoListDateTitle}>2025.01.07</div>
+          <RightArrow />
+        </div>
+      </div>
+      <div className={s.ToDoListMain}>
+        {todoItems.map((item) => (
+          <div
+            key={item.id}
+            className={s.ToDOListItem}
+            onClick={() => handleToggle(item.id)}
+          >
+            {item.checked ? <BtnToDoChecked /> : <BtnToDoNormal />}
+            <span className={s.ToDOListItemText}>{item.text}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
---
+++ src/features/home-todo/ui/style.css.ts
@@ -0,0 +1,71 @@
+import { style } from '@vanilla-extract/css';
+import { font } from 'shared/styles/font.css';
+import theme from 'shared/styles/theme.css';
+
+export const ToDoListContainer = style({
+  width: '268px',
+  minHeight: '278px',
+  padding: '8px 12px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '8px',
+  borderRadius: '12px',
+  alignSelf: 'stretch',
+  backgroundColor: theme.white,
+});
+
+export const ToDoListHeader = style({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'ceter',
+  justifyContent: 'space-between',
+});
+
+export const ToDoListTitle = style({
+  fontFamily: 'Pretendard',
+  fontSize: font.H5.fontSize,
+  fontWeight: font.H5.fontWeight,
+  lineHeight: font.H5.lineHeight,
+  fontStyle: 'normal',
+});
+
+export const ToDoListSetDate = style({
+  gap: '8px',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+});
+
+export const ToDoListDateTitle = style({
+  color: theme.gray[600],
+  fontFamily: 'Pretendard',
+  fontSize: font.p2.fontSize,
+  fontWeight: font.p2.fontWeight,
+  lineHeight: font.p2.lineHeight,
+  fontStyle: 'normal',
+});
+
+export const ToDoListMain = style({
+  display: 'flex',
+  paddingLeft: '8px',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '2px',
+});
+
+export const ToDOListItem = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+});
+
+export const ToDOListItemText = style({
+  color: theme.black,
+  fontFamily: 'Pretendard',
+  fontSize: font.p1.fontSize,
+  fontWeight: font.p1.fontWeight,
+  lineHeight: font.p1.lineHeight,
+  fontStyle: 'normal',
+});
