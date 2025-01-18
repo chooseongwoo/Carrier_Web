1e509601c459b70e44f800ee60b52540d19a7ea4
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-18 17:23:36 +0900

    chore(#6) :: ToDo 버튼

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as LeftArrow } from './ui/LeftArrow';
 export { default as RightArrow } from './ui/RightArrow';
+export { default as BtnToDoNormal } from './ui/BtnToDoNormal';
+export { default as BtnToDoChecked } from './ui/BtnToDoChecked';
---
+++ src/shared/icons/ui/BtnToDoNormal.tsx
@@ -0,0 +1,15 @@
+const BtnToDoNormal = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="10" cy="10.5" r="9.5" fill="white" stroke="#D8D8D8" />
+    </svg>
+  );
+};
+
+export default BtnToDoNormal;
