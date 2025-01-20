6cda4e34e4b29ae4e3346d33277871eb8bde414b
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-20 15:32:20 +0900

    feat(#6) :: Home 폴더 내에 컴포넌트 위치

--- src/pages/Home/ui/SideBar.tsx
+++ src/pages/Home/ui/SideBar.tsx
@@ -1,6 +1,6 @@
-import MiniCalendar from 'features/home-calendar/ui/MiniCalendar';
-import ToDoList from 'features/home-todo/ui/ToDoList';
-import Category from 'features/home-category/ui/Category';
+import MiniCalendar from 'features/Home/MiniCalendar/ui/MiniCalendar';
+import ToDoList from 'features/Home/Todo/ui/ToDoList';
+import Category from 'features/Home/Category/ui/Category';
 import * as s from './style.css';
 
 const SideBar = () => {
