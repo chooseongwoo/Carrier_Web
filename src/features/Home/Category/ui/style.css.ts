d057656c9748c278a3d207b1f55e500031f141b0
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-20 23:27:17 +0900

    feat(#6) :: 카테고리 요소 퍼블리싱

--- src/features/Home/Category/ui/style.css.ts
+++ src/features/Home/Category/ui/style.css.ts
@@ -7,7 +7,17 @@ export const CategoryContainer = style({
   padding: '8px 12px',
   flexDirection: 'column',
   alignItems: 'flex-start',
+  gap: '8px',
   flexShrink: '0',
+  alignSelf: 'stretch',
+  borderRadius: '12px',
+  backgroundColor: theme.white,
+});
+
+export const CategoryHeader = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   alignSelf: 'stretch',
 });
 
@@ -15,4 +25,52 @@ export const CategoryTitle = style({
   color: theme.gray[600],
   fontFamily: 'Pretendard',
   ...font.H5,
+});
+
+export const CategoryPlusBtn = style({
+  width: '24px',
+  height: '25px',
+  backgroundColor: theme.white,
+  border: 'none',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s ease',
+  borderRadius: '4px',
+
+  selectors: {
+    '&:hover': {
+      backgroundColor: theme.gray[50],
+    },
+  },
+});
+
+export const CategoryItemContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '2px',
+  alignSelf: 'stretch',
+});
+
+export const CategoryItem = style({
+  display: 'flex',
+  padding: '6px 8px',
+  alignItems: 'center',
+  gap: '8px',
+  alignSelf: 'stretch',
+  borderRadius: '4px',
+});
+
+export const CategoryItemTitle = style({
+  flex: '1 0 0',
+  color: theme.black,
+  fontFamily: 'Pretendard',
+  ...font.p1,
+});
+
+export const BtnCategoryItemStatic = style({
+  width: '24px',
+  height: '24px',
+  borderRadius: '4px',
+  border: `1px solid ${theme.gray[100]}`,
+  backgroundColor: '#EE3A3A',
 });
