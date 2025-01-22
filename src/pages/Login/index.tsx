47b13dd748912fbf3cae2c1179a670691f12255d
추성우 <cntjddn715@gmail.com>
2025-01-22 17:39:55 +0900

    feat(#9) :: 레이아웃 요소 배치

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,30 @@
+import {
+  AppTitle,
+  Google,
+  Asterisk,
+  Cube,
+  MobiusStrip,
+  Sphere,
+} from 'pages/Login/ui';
 import * as s from './style.css';
 
 const Login = () => {
-  return <main className={s.container}></main>;
+  return (
+    <main className={s.container}>
+      <img src={Asterisk} className={`${s.shape} ${s.Asterisk}`} />
+      <img src={Cube} className={`${s.shape} ${s.Cube}`} />
+      <img src={Sphere} className={`${s.shape} ${s.Sphere}`} />
+      <img src={MobiusStrip} className={`${s.shape} ${s.MobiusStrip}`} />
+      <div className={s.center}>
+        <AppTitle />
+        <p className={s.explain}>오늘의 작은 계획이 내일의 큰 꿈으로,</p>
+        <div className={s.loginButton}>
+          <Google />
+          <p className={s.loginText}>구글 로그인</p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
--- src/pages/Login/style.css.ts
+++ src/pages/Login/style.css.ts
@@ -1,3 +1,70 @@
 import { style } from '@vanilla-extract/css';
+import { font } from 'shared/styles/font.css';
+import theme from 'shared/styles/theme.css';
 
-export const container = style({});
+export const container = style({
+  position: 'relative',
+  display: 'flex',
+  width: '100vw',
+  height: '100vh',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: `linear-gradient(180deg, ${theme.white} 0%, #D2DCFF 100%)`,
+});
+
+export const center = style({
+  width: '520px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '16px',
+});
+
+export const explain = style({
+  color: `${theme.gray[600]}`,
+  ...font.H2,
+});
+
+export const loginButton = style({
+  marginTop: '24px',
+  padding: '11px 12px',
+  width: '336px',
+  display: 'flex',
+  justifyContent: 'start',
+  alignItems: 'center',
+  borderRadius: '4px',
+  background: theme.white,
+  cursor: 'pointer',
+  border: `1px solid ${theme.gray[200]}`,
+});
+
+export const loginText = style({
+  ...font.btn2,
+  color: theme.black,
+  width: '100%',
+  textAlign: 'center',
+});
+
+export const shape = style({
+  position: 'absolute',
+});
+
+export const Asterisk = style({
+  top: '5%',
+  left: '8%',
+});
+
+export const Cube = style({
+  right: '10%',
+  top: '10%',
+});
+
+export const Sphere = style({
+  bottom: '5%',
+  left: '6%',
+});
+
+export const MobiusStrip = style({
+  bottom: '5%',
+  right: '2%',
+});
