7c16d0b172eb725f51752015892352d0556169b7
Jamkris <dltmdgus1412@gmail.com>
2025-01-24 17:42:21 +0900

    feat(#13) :: Survey 페이지 라우팅 설정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
+
 import Login from 'pages/Login';
 import Home from 'pages/Home';
+import Survey from 'pages/Survey';
 
 export default function Router() {
   return (
@@ -10,6 +12,7 @@ export default function Router() {
         <Route path="/" element={<Home />}></Route>
       </Route>
       <Route path="/login" element={<Login />}></Route>
+      <Route path="/survey" element={<Survey />}></Route>
     </Routes>
   );
 }
