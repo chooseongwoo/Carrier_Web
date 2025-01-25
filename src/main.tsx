0201998fdcf389c79915ae9e44d827aa90ee83e5
추성우 <cntjddn715@gmail.com>
2025-01-25 16:20:53 +0900

    feat(#15) :: Tanstack Query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'shared/styles/global.css';
 import App from 'app/App';
+import ReactQueryProviders from 'app/providers/react-query-provider';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ReactQueryProviders>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReactQueryProviders>
 );
