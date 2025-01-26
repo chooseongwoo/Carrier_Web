5cc5df55de9d8ed4d2ece858bb8ffa162ad7a2ec
추성우 <cntjddn715@gmail.com>
2025-01-27 01:03:40 +0900

    refactor(#15) :: 쿼리 키 별도 관리

---
+++ src/features/auth/services/auth.keys.ts
@@ -0,0 +1,3 @@
+export const authKeys = {
+  loginLink: 'auth.query.loginLink',
+};
--- src/features/auth/services/auth.query.ts
+++ src/features/auth/services/auth.query.ts
@@ -1,10 +1,11 @@
 import { queryOptions } from '@tanstack/react-query';
 import { getLoginLink } from 'features/auth/services/auth.api';
+import { authKeys } from 'features/auth/services/auth.keys';
 
 export const authQuery = {
   loginLink: () =>
     queryOptions({
-      queryKey: ['query.loginLink'],
+      queryKey: [authKeys.loginLink],
       queryFn: getLoginLink,
     }),
 };
