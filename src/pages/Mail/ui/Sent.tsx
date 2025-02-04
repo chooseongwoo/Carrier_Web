06ca8d0f7f793376391cbd416ab832c79eb3a500
추성우 <cntjddn715@gmail.com>
2025-02-04 16:19:01 +0900

    chore(#42) :: 사이드바 아이콘 추가

---
+++ src/pages/Mail/ui/Sent.tsx
@@ -0,0 +1,24 @@
+const Sent = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Frame">
+        <path
+          id="Vector"
+          d="M6.00004 12L3.26904 3.125C9.80191 5.025 15.9624 8.02646 21.485 12C15.9627 15.9735 9.80257 18.9749 3.27004 20.875L6.00004 12ZM6.00004 12H13.5"
+          stroke="#2E5EFC"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default Sent;
