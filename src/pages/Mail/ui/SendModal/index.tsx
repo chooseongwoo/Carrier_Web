3b6b1f05f51f706dab83b371eb23db5f4d94b02d
추성우 <cntjddn715@gmail.com>
2025-02-05 17:02:43 +0900

    chore(#42) :: react-quill 라이브러리 설치

--- src/pages/Mail/ui/SendModal/index.tsx
+++ src/pages/Mail/ui/SendModal/index.tsx
@@ -1,7 +1,9 @@
 import { Sent, Cancel, Image, File } from 'pages/Mail/ui';
-
 import * as s from './style.css';
 import theme from 'shared/styles/theme.css';
+// @ts-ignore
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const SendModal = () => {
   return (
@@ -18,6 +20,7 @@ const SendModal = () => {
           <p className={s.label}>제목:</p>
           <input type="text" className={s.input} />
         </div>
+        <ReactQuill theme="snow" />
       </div>
       <footer className={s.footer}>
         <div className={`${s.icon}`}>
