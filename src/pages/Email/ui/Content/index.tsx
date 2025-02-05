fc72fe592913e601d08d74c247af146788982de4
Jamkris <dltmdgus1412@gmail.com>
2025-02-05 15:53:02 +0900

    feat(#35) :: 이메일 페이지 헤더 완성

--- src/pages/Email/ui/Content/index.tsx
+++ src/pages/Email/ui/Content/index.tsx
@@ -1,3 +1,5 @@
+import TrashcnaIcon from '../TrashcnaIcon';
+import WriteIcon from '../WriteIcon';
 import * as s from './style.css';
 
 const Content = () => {
@@ -8,7 +10,21 @@ const Content = () => {
           받은메일함
           <div className={s.subTitle}>4개의 메일, 1개 읽지 않음</div>
         </div>
+
+        <div className={s.mailOption}>
+          <div className={s.mailOption_addPlan}>일정으로 추가</div>
+          <div className={s.mailOption_write}>
+            <WriteIcon />
+          </div>
+          <div className={s.mailOption_delete}>
+            <TrashcnaIcon />
+          </div>
+        </div>
       </header>
+
+      <main>
+        <div></div>
+      </main>
     </div>
   );
 };
--- src/pages/Email/ui/Content/style.css.ts
+++ src/pages/Email/ui/Content/style.css.ts
@@ -2,7 +2,12 @@ import { style } from '@vanilla-extract/css';
 import { font } from 'shared/styles/font.css';
 import theme from 'shared/styles/theme.css';
 
-export const container = style({});
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: 'calc(100vw - 360px)',
+  height: '100vh',
+});
 
 export const header = style({
   width: 'calc(100vw - 360px)',
@@ -23,4 +28,38 @@ export const mailType = style({
 
 export const subTitle = style({
   ...font.p3,
+});
+
+export const mailOption = style({
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '12px',
+});
+
+export const mailOption_addPlan = style({
+  ...font.p1,
+  color: theme.blue[500],
+  marginRight: '12px',
+});
+
+export const mailOption_write = style({
+  width: '48px',
+  height: '48px',
+  backgroundColor: theme.gray[50],
+  borderRadius: '8px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const mailOption_delete = style({
+  width: '48px',
+  height: '48px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
 });
