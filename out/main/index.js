5a749588dbcf2d941f33fb818243dd3f4f82317e
Jamkris <dltmdgus1412@gmail.com>
2025-02-05 15:09:17 +0900

    chore(#35) :: Develop Pull Merge

--- out/main/index.js
+++ out/main/index.js
@@ -1,49 +1,49 @@
-import { app, BrowserWindow, shell } from 'electron';
-import path from 'path';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils';
-import __cjs_mod__ from 'node:module';
+import { app, BrowserWindow, shell } from "electron";
+import path from "path";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import __cjs_mod__ from "node:module";
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
 const require2 = __cjs_mod__.createRequire(import.meta.url);
 function createWindow() {
-  const preloadPath = path.join(__dirname, '../preload/index.mjs');
+  const preloadPath = path.join(__dirname, "../preload/index.mjs");
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 640,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? {} : {}),
+    ...process.platform === "linux" ? {} : {},
     webPreferences: {
       preload: preloadPath,
       contextIsolation: true,
-      nodeIntegration: true,
-    },
+      nodeIntegration: true
+    }
   });
-  mainWindow.on('ready-to-show', () => {
+  mainWindow.on("ready-to-show", () => {
     mainWindow.show();
   });
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url);
-    return { action: 'deny' };
+    return { action: "deny" };
   });
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+    mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
   }
 }
 app.whenReady().then(() => {
-  electronApp.setAppUserModelId('com.electron');
-  app.on('browser-window-created', (_, window) => {
+  electronApp.setAppUserModelId("com.electron");
+  app.on("browser-window-created", (_, window) => {
     optimizer.watchWindowShortcuts(window);
   });
   createWindow();
-  app.on('activate', function () {
+  app.on("activate", function() {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
--- out/preload/index.mjs
+++ out/preload/index.mjs
@@ -1,13 +1,15 @@
-import { contextBridge } from 'electron';
-import { electronAPI } from '@electron-toolkit/preload';
+import { contextBridge } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
 const api = {};
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI);
-    contextBridge.exposeInMainWorld('api', api);
+    contextBridge.exposeInMainWorld("electron", electronAPI);
+    contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
-    console.error('Failed to expose Electron API in the renderer:', error);
-    console.error(error);
+    console.error(
+      "renderer에서 Electron API를 호출하는 데 실패했습니다:",
+      error
+    );
   }
 } else {
   window.electron = electronAPI;
