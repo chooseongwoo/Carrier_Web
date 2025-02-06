caa2b51ccf45d83d05123aeb72e62153d0df2a37
Jamkris <dltmdgus1412@gmail.com>
2025-02-06 15:17:34 +0900

    feat(#35) :: 라우팅 설정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,14 +5,14 @@ import Login from 'pages/Login';
 import Home from 'pages/Home';
 import Survey from 'pages/Survey';
 import OAuth from 'pages/OAuth';
-import Email from 'pages/Email';
+import Mail from 'pages/Mail';
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/mail" element={<Email />} />
+        <Route path="/mail" element={<Mail />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/survey" element={<Survey />} />
