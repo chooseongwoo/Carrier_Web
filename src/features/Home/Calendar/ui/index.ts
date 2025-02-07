a8b5ff6406881ef773d1c5ede7b1c9ba2c358de2
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-07 22:30:14 +0900

    feat(#11) :: 캘린더 이벤트 icon 추가

--- src/features/Home/Calendar/ui/index.ts
+++ src/features/Home/Calendar/ui/index.ts
@@ -1,2 +1,4 @@
 export { default as CalendarPlus } from './CalendarPlus.tsx';
 export { default as CalendarSearch } from './CalendarSearch.tsx';
+export { default as CalendarSchedule } from './CalendarSchedule.tsx';
+export { default as CalendarTodo } from './CalendarTodo.tsx';
