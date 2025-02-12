d87dee7b56bc88960a7979a3094e7b76a3749ed9
추성우 <cntjddn715@gmail.com>
2025-02-13 00:13:37 +0900

    fix(#42) :: package.json과 pnpm-lock 버전 일치

--- out/preload/index.mjs
+++ out/preload/index.mjs
@@ -1,13 +1,13 @@
-import { contextBridge } from "electron";
-import { electronAPI } from "@electron-toolkit/preload";
+import { contextBridge } from 'electron';
+import { electronAPI } from '@electron-toolkit/preload';
 const api = {};
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld("electron", electronAPI);
-    contextBridge.exposeInMainWorld("api", api);
+    contextBridge.exposeInMainWorld('electron', electronAPI);
+    contextBridge.exposeInMainWorld('api', api);
   } catch (error) {
     console.error(
-      "renderer에서 Electron API를 호출하는 데 실패했습니다:",
+      'renderer에서 Electron API를 호출하는 데 실패했습니다:',
       error
     );
   }
