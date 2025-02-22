b05a0d7fa7e031abac2d6d431136bcff82668ae1
추성우 <cntjddn715@gmail.com>
2025-02-22 17:49:37 +0900

    feat(#60) :: 웹/앱 router 동작 방식 지정
    웹에서는 BrowserRouter, 앱에서는 HashRouter를 사용하여 라우팅 되도록 했습니다

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import 'shared/styles/global.css';
 import App from 'app/App';
 import ReactQueryProviders from 'app/providers/react-query-provider';
 
+const isElectron = window.navigator.userAgent
+  .toLowerCase()
+  .includes('electron');
+
+const Router = isElectron ? HashRouter : BrowserRouter;
+
 createRoot(document.getElementById('root')!).render(
   <ReactQueryProviders>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </ReactQueryProviders>
 );
