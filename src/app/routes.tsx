cd52602eff117c9ce6cd321cd387dc4f402ef4e2
추성우 <cntjddn715@gmail.com>
2025-03-01 00:55:47 +0900

    feat(#72) :: 로그인 여부에 따른 리다이렉트

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,20 +1,53 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './Layout';
-
 import Login from 'pages/Login';
 import Home from 'pages/Home';
 import Survey from 'pages/Survey';
 import OAuth from 'pages/OAuth';
 import Mail from 'pages/Mail';
+import useUser from 'entities/user/hooks/useUser';
+
+interface RouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ children }: RouteProps) => {
+  const { isLoggedIn } = useUser();
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
+
+const PublicRoute = ({ children }: RouteProps) => {
+  const { isLoggedIn } = useUser();
+  if (isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
 
 export default function Router() {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
         <Route path="/" element={<Home />} />
         <Route path="/mail" element={<Mail />} />
       </Route>
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
       <Route path="/survey" element={<Survey />} />
       <Route path="/google/callback" element={<OAuth />} />
     </Routes>
