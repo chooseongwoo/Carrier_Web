13a607735c94a32665a6a1dd25e8a86064a03cd0
추성우 <cntjddn715@gmail.com>
2025-03-02 22:40:32 +0900

    chore(#72) :: 잘못된 페이지 import 제거

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,7 +6,6 @@ import Home from 'pages/Home';
 import Survey from 'pages/Survey';
 import OAuth from 'pages/OAuth';
 import Mail from 'pages/Mail';
-import Setting from 'pages/Setting';
 import useUser from 'entities/user/hooks/useUser';
 import { DotLoader } from 'react-spinners';
 import theme from 'shared/styles/theme.css';
@@ -43,7 +42,6 @@ export default function Router() {
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/mail" element={<Mail />} />
-            <Route path="/setting" element={<Setting />} />
           </Route>
         </Route>
 
