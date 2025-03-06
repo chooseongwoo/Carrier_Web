259df122d408bb518deea1786b5ce43607c73fe5
SeungHyun <dltmdgus1412@gmail.com>
2025-03-06 15:08:57 +0900

    Update auth.mutation.ts

--- src/features/auth/services/auth.mutation.ts
+++ src/features/auth/services/auth.mutation.ts
@@ -2,14 +2,21 @@ import { useMutation } from '@tanstack/react-query';
 import { Storage } from 'shared/lib/storage';
 import { TOKEN } from 'shared/constants';
 import { postLogin } from './auth.api';
+import { useNavigate } from 'react-router-dom';
 
 export const useLoginMutation = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: postLogin,
     onSuccess: ({ accessToken, refreshToken }) => {
       Storage.setItem(TOKEN.ACCESS, accessToken);
       Storage.setItem(TOKEN.REFRESH, refreshToken);
-      window.location.href = '/';
+      console.error('로그인 중 에러 발생:', error);
+      navigate('/');
+    },
+    onError: (error) => {
+      console.error('로그인 중 에러 발생:', error);
     },
   });
 };
