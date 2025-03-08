3355a6b95d6e0a811c7fb6e2baf127088446b99e
Jamkris <dltmdgus1412@gmail.com>
2025-03-09 02:12:35 +0900

    feat(#88) :: CloseIcon 추가

---
+++ src/features/Home/TipsModal/ui/CloseIcon.tsx
@@ -0,0 +1,21 @@
+const CloseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+    >
+      <path
+        d="M8 24.5L24 8.5M8 8.5L24 24.5"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
