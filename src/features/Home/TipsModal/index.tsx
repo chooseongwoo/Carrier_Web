745921bc216dbde1a052909e877bf610c187ddb6
Jamkris <dltmdgus1412@gmail.com>
2025-03-09 02:57:02 +0900

    feat(#88) :: 팁 모달 일정 부분 완성

--- src/features/Home/TipsModal/index.tsx
+++ src/features/Home/TipsModal/index.tsx
@@ -1,11 +1,114 @@
+import { title } from 'process';
 import * as s from './style.css';
+import CloseIcon from './ui/CloseIcon';
+
+const CategoryData = [
+  {
+    title: '롤 실버가기',
+    color: '#B6A917',
+    start: '',
+    end: '',
+  },
+  {
+    title: '오징징이들 회의',
+    color: '#2E5EFC',
+    start: '9:30 AM',
+    end: '3:10 PM',
+  },
+  {
+    title: '롤 실버가기',
+    color: '#B26C1B',
+    start: '9:30 AM',
+    end: '3:10 PM',
+  },
+  {
+    title: '롤 실버가기',
+    color: '#B14FDE',
+    start: '9:30 AM',
+    end: '3:10 PM',
+  },
+  {
+    title: '롤 실버가기',
+    color: '#15A665',
+    start: '9:30 AM',
+    end: '3:10 PM',
+  },
+  {
+    title: '롤 실버가기',
+    color: '#D63881',
+    start: '9:30 AM',
+    end: '3:10 PM',
+  },
+];
 
 const TipsModal = () => {
   return (
     <div className={s.TipsModal_Background}>
       <div className={s.TipsModal_Container}>
-        <div>
-          <div></div>
+        <div
+          className={s.TipsModal_Header}
+          style={{ backgroundImage: 'url(/images/HeaderImg.png)' }}
+        >
+          <div className={s.TipsModal_CloseIcon}>
+            <CloseIcon />
+          </div>
+          <div className={s.TipsModal_Title}>오늘의 일정 🗓️</div>
+        </div>
+
+        <div className={s.TipsModal_CategoryTodo}>
+          <div className={s.TipsModal_Category}>
+            {/* Map 돌 */}
+
+            {CategoryData.map((item) => {
+              return (
+                <div
+                  className={s.Category_Content}
+                  style={{ backgroundColor: item.color }}
+                >
+                  <div className={s.Category_Color} />
+                  <div
+                    className={s.Category_Title}
+                    style={{
+                      backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                      color: item.color,
+                    }}
+                  >
+                    {item.title}
+
+                    {item.start && item.end && (
+                      <div>
+                        {item?.start} ~ {item?.end}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+
+            <div
+              className={s.Category_Content}
+              style={{ backgroundColor: '#B6A917' }}
+            >
+              <div className={s.Category_Color} />
+              <div
+                className={s.Category_Title}
+                style={{
+                  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                  color: '#B6A917',
+                }}
+              >
+                롤 실버가기
+                <div>9:30 AM ~ 3:10 PM</div>
+              </div>
+            </div>
+
+            {/* Map 돌 */}
+          </div>
+          <div className={s.TipsModal_Todo}>
+            {/* Map 돌 */}
+
+            {/* Map 돌 */}
+          </div>
         </div>
       </div>
     </div>
--- src/features/Home/TipsModal/style.css.ts
+++ src/features/Home/TipsModal/style.css.ts
@@ -6,19 +6,92 @@ export const TipsModal_Background = style({
   width: '100%',
   height: '100%',
   position: 'absolute',
-  zIndex: 1,
+  zIndex: 10,
   backgroundColor: 'rgba(18, 18, 19, 0.20);',
 });
 
 export const TipsModal_Container = style({
-  ...font.H3,
-  width: '800px',
+  width: '48rem',
   height: '752px',
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  zIndex: 2,
+  zIndex: 11,
   borderRadius: '12px',
   backgroundColor: theme.white,
+});
+
+export const TipsModal_Header = style({
+  width: '100%',
+  height: '146px',
+  borderRadius: '12px 12px 0 0',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+});
+
+export const TipsModal_CloseIcon = style({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  padding: '8px 8px 0 0',
+});
+
+export const TipsModal_Title = style({
+  ...font.H3,
+  width: '100%',
+  color: theme.white,
+  padding: '0 0 16px 28px',
+});
+
+export const TipsModal_CategoryTodo = style({
+  width: '100%',
+  height: '250px',
+  padding: '24px 28px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+});
+
+export const TipsModal_Category = style({
+  width: 'calc(50% - 4px)',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+  overflow: 'scroll',
+});
+
+export const Category_Content = style({
+  width: '100%',
+  minHeight: '30px',
+  height: '30px',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'start',
+  alignItems: 'center',
+  borderRadius: '6px',
+});
+
+export const Category_Color = style({
+  width: '4px',
+  height: '100%',
+  borderRadius: '6px 0 0 6px',
+});
+
+export const Category_Title = style({
+  ...font.p2,
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderRadius: '0 6px 6px 0',
+  padding: '0 16px 0px 8px ',
+});
+
+export const TipsModal_Todo = style({
+  width: 'calc(50% - 4px)',
 });
