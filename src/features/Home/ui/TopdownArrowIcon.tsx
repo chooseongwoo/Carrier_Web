a9317d4c3e232167776c3537ebc763971ca2e8b4
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-10 09:08:20 +0900

    feat(#90) :: TopdownArrowIcon 추가

---
+++ src/features/Home/ui/TopdownArrowIcon.tsx
@@ -0,0 +1,26 @@
+const TopdownArrowIcon = () => (
+  <svg
+    width="10"
+    height="18"
+    viewBox="0 0 10 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.875 6.5625L5 3.4375L8.125 6.5625"
+      stroke="#323131"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.125 11.4375L5 14.5625L1.875 11.4375"
+      stroke="#323131"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default TopdownArrowIcon;
