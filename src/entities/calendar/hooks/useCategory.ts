5e1e6bb90cbb29e2fe534eb3ca313eba4ded5768
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-11 22:11:56 +0900

    feat(#95) :: TanstackQuery에서 useEffect 사용 지양

--- src/entities/calendar/hooks/useCategory.ts
+++ src/entities/calendar/hooks/useCategory.ts
@@ -2,18 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { useSetAtom, useAtomValue } from 'jotai';
 import { useCategoryListQuery } from 'features/Home/services/home.query';
 import { categoriesAtom } from '../contexts/category';
-import { useEffect } from 'react';
 
 export const useCategoryList = () => {
   const setCategories = useSetAtom(categoriesAtom);
 
   const query = useQuery(useCategoryListQuery.getCategoryList());
 
-  useEffect(() => {
-    if (query.data) {
-      setCategories(query.data);
-    }
-  }, [query.data, setCategories]);
+  if (query.data) {
+    setCategories(query.data);
+  }
 
   return query;
 };
