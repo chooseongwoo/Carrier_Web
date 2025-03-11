a3bfcb9f49aebf8055e886ee5edb16c4ef74dcb7
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-11 21:20:57 +0900

    feat(#95) :: 카테고리 전역 상태 관리 hook useEffect 적용

--- src/entities/calendar/hooks/useCategory.ts
+++ src/entities/calendar/hooks/useCategory.ts
@@ -2,15 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 import { useSetAtom, useAtomValue } from 'jotai';
 import { useCategoryListQuery } from 'features/Home/services/home.query';
 import { categoriesAtom } from '../contexts/category';
+import { useEffect } from 'react';
 
 export const useCategoryList = () => {
   const setCategories = useSetAtom(categoriesAtom);
 
   const query = useQuery(useCategoryListQuery.getCategoryList());
 
-  if (query.data) {
-    setCategories(query.data);
-  }
+  useEffect(() => {
+    if (query.data) {
+      setCategories(query.data);
+    }
+  }, [query.data, setCategories]);
 
   return query;
 };
