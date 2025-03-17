001f56e3102e27c8b0514bd9f2b0d12147f91ff9
추성우 <cntjddn715@gmail.com>
2025-03-17 16:48:41 +0900

    feat(#108) :: 프로필 사진 변경 mutation 생성

--- src/features/user/services/user.mutation.ts
+++ src/features/user/services/user.mutation.ts
@@ -1,6 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
-import { patchUserInfo } from 'features/user/services/user.api';
+import {
+  patchUserInfo,
+  patchUserPicture,
+} from 'features/user/services/user.api';
 
 export const useUpdateUserInfo = () => {
   return useMutation({ mutationFn: patchUserInfo });
+};
+
+export const useUpdateUserPictrue = () => {
+  return useMutation({ mutationFn: patchUserPicture });
 };
