c81d536b6b52f2a78d2ab17cb17812ebb2710df0
추성우 <cntjddn715@gmail.com>
2025-03-25 00:10:04 +0900

    chore(#122) :: 파일명 수정

--- src/pages/Login/utils/getDownloadUrl.ts
+++ src/pages/Login/utils/getDownloadUrl.ts
@@ -2,11 +2,11 @@ export const getDownloadUrl = (): string | null => {
   const platform = navigator.platform.toLowerCase();
 
   if (platform.includes('mac')) {
-    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/app.dmg';
+    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.dmg';
   }
 
   if (platform.includes('win')) {
-    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/app.exe';
+    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.exe';
   }
 
   return null;
