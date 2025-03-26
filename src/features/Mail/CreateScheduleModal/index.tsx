7c4f7b4196e2407b483345f2e8aefdb5f086c97e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-26 22:36:41 +0900

    chore(#127) :: 잘못된 value 변경

--- src/features/Mail/CreateScheduleModal/index.tsx
+++ src/features/Mail/CreateScheduleModal/index.tsx
@@ -190,7 +190,7 @@ const CreateScheduleModal = ({ toggleModalClose }: MailModalProps) => {
                     id={state.selectedPriorityId}
                     data={[
                       { id: 1, value: 'LOW', name: '낮음' },
-                      { id: 2, value: 'MIDDLE', name: '중간' },
+                      { id: 2, value: 'MEDIUM', name: '중간' },
                       { id: 3, value: 'HIGH', name: '높음' },
                     ]}
                     onChange={handleChangePriority}
