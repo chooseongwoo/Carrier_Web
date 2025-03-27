c1d7c71ec8d4623c049f857fabdad4b4fad10792
Jamkris <dltmdgus1412@gmail.com>
2025-03-27 14:09:42 +0900

    feat(#128) :: 좌우 레이아웃 수정

--- src/features/Proceed/Content/index.tsx
+++ src/features/Proceed/Content/index.tsx
@@ -1,7 +1,14 @@
 import * as s from './style.css';
 
 const ProceedContent = () => {
-  return <div className={s.container}>lkjljlkjsaldfjljf</div>;
+  return (
+    <div className={s.container}>
+      <div className={s.sidebar}>
+        <div className={s.title}>모든 녹음 요약</div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProceedContent;
--- src/features/Proceed/Content/style.css.ts
+++ src/features/Proceed/Content/style.css.ts
@@ -5,3 +5,14 @@ export const container = style({
   height: '100vh',
   backgroundColor: 'red',
 });
+
+export const sidebar = style({
+  width: '400px',
+  height: '100%',
+  backgroundColor: 'blue',
+});
+
+export const title = style({
+  width: '100%',
+  height: '72px',
+});
