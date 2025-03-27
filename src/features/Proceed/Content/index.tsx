fe25cd46a47a196e167d162b89759784d63c96df
Jamkris <dltmdgus1412@gmail.com>
2025-03-28 08:43:28 +0900

    feat(#128) :: 사이드바 구성 완료

--- src/features/Proceed/Content/index.tsx
+++ src/features/Proceed/Content/index.tsx
@@ -5,6 +5,25 @@ const ProceedContent = () => {
     <div className={s.container}>
       <div className={s.sidebar}>
         <div className={s.title}>모든 녹음 요약</div>
+        <div className={s.recordContents}>
+          {/* Map */}
+
+          <div className={s.recordContent}>
+            <div className={s.recordTitle}>
+              <div className={s.recordTitleText}>제목</div>
+              <div className={s.recordTitleDate}>2025.02.30</div>
+            </div>
+
+            <div className={s.recordTime}>00.00초</div>
+          </div>
+
+          {/* Map */}
+        </div>
+        <div className={s.recordButtonLayout}>
+          <div className={s.recordButton}>
+            <div className={s.recordButtonInner} />
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
--- src/features/Proceed/Content/style.css.ts
+++ src/features/Proceed/Content/style.css.ts
@@ -1,18 +1,94 @@
 import { style } from '@vanilla-extract/css';
+import { font } from 'shared/styles/font.css';
+import theme from 'shared/styles/theme.css';
 
 export const container = style({
   width: 'calc(100vw - 80px)',
   height: '100vh',
-  backgroundColor: 'red',
 });
 
 export const sidebar = style({
   width: '400px',
   height: '100%',
-  backgroundColor: 'blue',
+  borderRight: `1px solid ${theme.gray[100]}`,
 });
 
 export const title = style({
+  ...font.H4,
   width: '100%',
   height: '72px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderBottom: `1px solid ${theme.gray[100]}`,
+});
+
+export const recordContents = style({
+  width: '100%',
+  height: 'calc(100% - 160px)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'start',
+  overflowY: 'scroll',
+  padding: '0 20px',
+});
+
+export const recordContent = style({
+  width: '100%',
+  borderBottom: `1px solid ${theme.gray[100]}`,
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  padding: '20px 12px',
+});
+
+export const recordTitle = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+});
+
+export const recordTitleText = style({
+  ...font.H4,
+});
+
+export const recordTitleDate = style({
+  ...font.p2,
+  color: theme.gray[600],
+});
+
+export const recordTime = style({
+  ...font.p2,
+  color: theme.gray[600],
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'end',
+});
+
+export const recordButtonLayout = style({
+  width: '100%',
+  height: '88px',
+  backgroundColor: theme.gray[50],
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const recordButton = style({
+  width: '48px',
+  height: '48px',
+  backgroundColor: theme.white,
+  borderRadius: '100%',
+  outline: `3px solid ${theme.gray[400]}`,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+export const recordButtonInner = style({
+  width: '44px',
+  height: '44px',
+  borderRadius: '100%',
+  backgroundColor: theme.red[500],
 });
