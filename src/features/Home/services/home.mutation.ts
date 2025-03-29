70b7b7998bb40948a2cd0f3714dcfec732b35230
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-29 15:42:44 +0900

    feat(#127) :: CUD 작동 시 재조회

--- src/features/Home/services/home.mutation.ts
+++ src/features/Home/services/home.mutation.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import {
   patchCategory,
   patchTodoState,
@@ -10,28 +10,57 @@ import {
   patchTodo,
   deleteTodo,
 } from './home.api';
+import { homeKeys } from './home.keys';
 
 export const useCreateTodoMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.TODO_LIST],
+      });
+    },
   });
 };
 
 export const usePatchTodoMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.TODO_LIST],
+      });
+    },
   });
 };
 
 export const usePatchTodoStateMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchTodoState,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.TODO_LIST],
+      });
+    },
   });
 };
 
 export const useDeleteTodoMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.TODO_LIST],
+      });
+    },
   });
 };
 
@@ -48,19 +77,40 @@ export const useCreateCategoryMutation = () => {
 };
 
 export const useCreateScheduleMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postSchedule,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.SCHEDULE_LIST],
+      });
+    },
   });
 };
 
 export const usePatchScheduleMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchSchedule,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.SCHEDULE_LIST],
+      });
+    },
   });
 };
 
 export const useDeleteScheduleMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteSchedule,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [homeKeys.SCHEDULE_LIST],
+      });
+    },
   });
 };
