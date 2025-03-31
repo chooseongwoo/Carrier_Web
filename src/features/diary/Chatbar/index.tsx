b3bcaf497fef9c6af3f05f66c180d6e628f174ec
seonghoon07 <chltjdgns1009@gmail.com>
2025-03-31 23:30:45 +0900

    feat(#134) :: 일기 주제 추천 api 연동

--- src/features/diary/Chatbar/index.tsx
+++ src/features/diary/Chatbar/index.tsx
@@ -1,23 +1,41 @@
 import * as s from './style.css';
+import { useQuery } from '@tanstack/react-query';
+import { useDiaryQuery } from 'features/diary/services/diary.query.ts';
+import { NowDateDash } from 'shared/lib/date';
 
 const Chatbar = () => {
+  const {
+    data: recommendData,
+    refetch,
+    isFetching,
+    isError,
+    isFetched,
+  } = useQuery({
+    ...useDiaryQuery.getSubject(NowDateDash),
+    enabled: false,
+  });
+
+  const handleRecommendClick = async () => {
+    try {
+      await refetch();
+    } catch {}
+  };
+
   return (
     <div className={s.container}>
-      <div className={s.suggestionList}>
-        <div className={s.suggestionText}>일정 주제 질문</div>
-      </div>
-      <div className={s.suggestionList}>
-        <div className={s.suggestionText}>
-          아침에 일어났을 땐 평소와 달랐나요?
+      {isError && <div>데이터를 불러오는 데 실패했습니다.</div>}
+      {isFetched && recommendData?.recommend?.length === 0 && (
+        <div>오늘은 추천드릴 내용이 없습니다!</div>
+      )}
+      {recommendData?.recommend.map((subject: string) => (
+        <div className={s.suggestionList}>
+          <div className={s.suggestionText}>{subject}</div>
         </div>
-      </div>
-      <div className={s.suggestionList}>
-        <div className={s.suggestionText}>
-          오후 4시 30분에 있었던 화영 거래처와의 미팅은 어떠셨나요?
+      ))}
+      <button className={s.suggestionButton} onClick={handleRecommendClick}>
+        <div className={s.suggestionButtonText}>
+          {isFetching ? '로딩 중' : '일기 주제 추천받기'}
         </div>
-      </div>
-      <button className={s.suggestionButton}>
-        <div className={s.suggestionButtonText}>일기 주제 추천받기</div>
       </button>
     </div>
   );
