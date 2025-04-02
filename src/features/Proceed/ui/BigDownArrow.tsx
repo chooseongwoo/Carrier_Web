a6fc71f71e27f2c8ad4b56a1270c457db26bcfbd
Jamkris <dltmdgus1412@gmail.com>
2025-04-02 22:31:58 +0900

    feat(#128) :: 본문 아래 방향표시 아이콘 추가

---
+++ src/features/Proceed/ui/BigDownArrow.tsx
@@ -0,0 +1,33 @@
+const BigDownArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="68"
+      height="39"
+      viewBox="0 0 68 39"
+      fill="none"
+    >
+      <g clip-path="url(#clip0_2162_18644)">
+        <path
+          d="M12.75 13.4062L34 25.5938L55.25 13.4062"
+          stroke="#787878"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_2162_18644">
+          <rect
+            width="39"
+            height="68"
+            fill="white"
+            transform="matrix(0 -1 -1 0 68 39)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default BigDownArrow;
