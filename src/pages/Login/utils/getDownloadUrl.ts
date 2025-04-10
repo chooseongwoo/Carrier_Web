c16bd637fa1d10bbf8a0c5d585dec08fc03de755
추성우 <cntjddn715@gmail.com>
2025-04-11 08:43:48 +0900

    chore(#167) :: 설치 파일명 변경

--- src/pages/Login/utils/getDownloadUrl.ts
+++ src/pages/Login/utils/getDownloadUrl.ts
@@ -2,11 +2,11 @@ export const getDownloadUrl = (): string | null => {
   const platform = navigator.platform.toLowerCase();
 
   if (platform.includes('mac')) {
-    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.dmg';
+    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.for.mac.zip';
   }
 
   if (platform.includes('win')) {
-    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.exe';
+    return 'https://github.com/Jing5s/Carrier_Web/releases/latest/download/Carrier.for.win.zip';
   }
 
   return null;
